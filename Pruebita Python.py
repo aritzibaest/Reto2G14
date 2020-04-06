2eea1e1313b27846403d86d807676ba8bb334235
Ane Sanchiz <ane.sanchiz@opendeusto.es>
2020-04-06 19:21:56 +0200

    Prueba Clon

---
+++ Pruebita Python.py
@@ -0,0 +1,28 @@
+
+
+x = "hola"
+print(x)
+
+if x != "hola":
+    print("adios")
+
+contador = 0
+while (contador < 3):
+    contador = contador +1
+    if contador == 2:
+        #El continue hace que se salte lo de debajo y continue el while, Break sale.
+        continue
+    print("Aqui, iterando")
+
+print("FIN")
+
+
+
+for i in range (3)
+    print(i)
+
+
+
+
+
+
